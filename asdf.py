23a635b0978c032442b87b22e36187399fc8b38d
soryrawyer <sory.rawyer@gmail.com>
2018-05-31 17:59:02 -0400

    take 2 Co-authored-by: Ashley Blewer <ashley.blewer@gmail.com>

--- asdf.py
+++ asdf.py
@@ -5,3 +5,5 @@ asdf.py : a test for my little todo webhook
 # TODO: test 3 fix the todo app thing to make sure it doesn't re-add stuff every time
 
 # TODO: don't make duplicates again
+
+# TODO: live demo
